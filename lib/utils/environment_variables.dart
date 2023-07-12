ba3271306dbca1cf64024519dfa99471b8f2e5ce
Norihide Oda <60128781+Aosanori@users.noreply.github.com>
2023-07-12 15:25:38 +0900

    :hammer: 検索URLの生成部分に空白をクエリ用に+に変換する処理の追加

--- lib/utils/environment_variables.dart
+++ lib/utils/environment_variables.dart
@@ -1,4 +1,18 @@
 // ignore_for_file: public_member_api_docs
 
-String generateURLFromSearchQuery(String repositoryQuery, int page, int itemsPerPage) =>
-    'https://api.github.com/search/repositories?q=$repositoryQuery&page=$page&per_page=$itemsPerPage';
+String generateURLFromSearchQuery(
+  String repositoryQuery,
+  int page,
+  int itemsPerPage,
+) {
+  // 空白をクエリ用に+に変換する処理
+  if (repositoryQuery.substring(0, 0).isEmpty) {
+    final query = repositoryQuery.replaceFirst(' ', '');
+    repositoryQuery = query;
+  }
+  if (repositoryQuery.contains(' ')) {
+    final query = repositoryQuery.replaceAll(' ', '+');
+    repositoryQuery = query;
+  }
+  return 'https://api.github.com/search/repositories?q=$repositoryQuery&page=$page&per_page=$itemsPerPage';
+}
