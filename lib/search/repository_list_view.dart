ed4615e12e9d1baf8606ceb73a4bdbfc4b8f0634
Norihide Oda <60128781+Aosanori@users.noreply.github.com>
2023-07-12 20:53:45 +0900

    :hammer: RepositoryListViewの切り出しの粒度を変更

--- lib/search/repository_list_view.dart
+++ lib/search/repository_list_view.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
-import '../repository_overview/repository_overview.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+
+import '../components/network_error_widget.dart';
+import '../components/no_result_widget.dart';
+import '../components/unknown_error_widget.dart';
 import 'repository_list_tile.dart';
+import 'search_repository_state_notifier.dart';
 
-
-class RepositoryListView extends StatelessWidget {
-  const RepositoryListView(this.repositoryList, {super.key});
-  final List<RepositoryOverview> repositoryList;
+class RepositoryListView extends ConsumerWidget {
+  const RepositoryListView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: repositoryList.length,
-      itemBuilder: (context, index) {
-        return RepositoryListTile(repositoryList[index]);
-      },
+  Widget build(BuildContext context, WidgetRef ref) {
+    final searchRepositoryOverviewState =
+        ref.watch(searchRepositoryStateNotifierProvider);
+    return Expanded(
+      child: Container(
+        child: searchRepositoryOverviewState.when(
+          data: (repositoryList) => LazyLoadScrollView(
+            onEndOfPage: () {
+              final notifier = ref.read(
+                searchRepositoryStateNotifierProvider.notifier,
+              );
+              notifier.searchRepositoryOverview();
+            },
+            child: repositoryList.isNotEmpty
+                ? ListView.builder(
+                    itemCount: repositoryList.length,
+                    itemBuilder: (context, index) {
+                      return RepositoryListTile(repositoryList[index]);
+                    },
+                  )
+                : const Center(child: NoResultsWidget()),
+          ),
+          loading: () => const Center(child: CircularProgressIndicator()),
+          error: (error, stackTrace) => error is http.ClientException
+              ? const Center(child: NetworkErrorWidget())
+              : const Center(child: UnknownErrorWidget()),
+        ),
+      ),
     );
   }
 }
--- lib/search/search_repository_page.dart
+++ lib/search/search_repository_page.dart
@@ -1,52 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart' as http;
-import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
 
-import '../components/network_error_widget.dart';
-import '../components/no_result_widget.dart';
-import '../components/unknown_error_widget.dart';
 import 'repository_list_view.dart';
 import 'search_repository_query_form.dart';
-import 'search_repository_state_notifier.dart';
 
-class SearchRepositoryPage extends ConsumerWidget {
+class SearchRepositoryPage extends StatelessWidget {
   const SearchRepositoryPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final searchRepositoryOverviewState =
-        ref.watch(searchRepositoryStateNotifierProvider);
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expanded Column Sample'),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           children: <Widget>[
-            const SearchRepositoryQueryForm(),
-            Expanded(
-              child: Container(
-                child: searchRepositoryOverviewState.when(
-                  data: (repositoryList) => LazyLoadScrollView(
-                    onEndOfPage: () {
-                      final notifier = ref.read(
-                        searchRepositoryStateNotifierProvider.notifier,
-                      );
-                      notifier.searchRepositoryOverview();
-                    },
-                    child: repositoryList.isNotEmpty
-                        ? RepositoryListView(repositoryList)
-                        : const Center(child: NoResultsWidget()),
-                  ),
-                  loading: () =>
-                      const Center(child: CircularProgressIndicator()),
-                  error: (error, stackTrace) => error is http.ClientException
-                      ? const Center(child: NetworkErrorWidget())
-                      : const Center(child: UnknownErrorWidget()),
-                ),
-              ),
-            ),
+            SearchRepositoryQueryForm(),
+            RepositoryListView(),
           ],
         ),
       ),
