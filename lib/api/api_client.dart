a421c9000a083f8345a1b616c131d2bc53107d8f
Norihide Oda <60128781+Aosanori@users.noreply.github.com>
2023-07-12 15:26:03 +0900

    :hammer: APIの呼び出し部分を切り出し

---
+++ lib/api/api_client.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: public_member_api_docs
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+final apiClientProvider = Provider((ref) => APIClient());
+
+class APIClient {
+  Future<Map<String, dynamic>> fetchData(String url) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return data as Map<String, dynamic>;
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } on http.ClientException catch (e) {
+      debugPrint('Network error: $e');
+    } catch (e) {
+      debugPrint('Unknown error: $e');
+    }
+    return {};
+  }
+}
--- lib/repository_overview/repository_overview_data_source.dart
+++ lib/repository_overview/repository_overview_data_source.dart
@@ -1,40 +1,28 @@
 // ignore_for_file: non_constant_identifier_names, public_member_api_docs
-import 'dart:convert';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart' as http;
 
+import '../api/api_client.dart';
 import '../utils/environment_variables.dart';
 import 'repository_overview.dart';
 
-final repositoryOverviewDataSourceProvider =
-    Provider((ref) => RepositoryOverviewDataSource());
+final repositoryOverviewDataSourceProvider = Provider(
+  (ref) => RepositoryOverviewDataSource(
+    ref.read(apiClientProvider),
+  ),
+);
 
 class RepositoryOverviewDataSource {
+  RepositoryOverviewDataSource(this.apiClient);
+  final APIClient apiClient;
+
   Future<List<RepositoryOverview>> searchRepositoryOverviewWithCursor(
     String repositoryQuery,
     int page,
     int itemsPerPage,
   ) async {
-    // 空白をクエリ用に+に変換する処理
-    // if (repositoryQuery.substring(0, 0).isEmpty) {
-    //   final query = repositoryQuery.replaceFirst(' ', '');
-    //   repositoryQuery = query;
-    // }
-    // if (repositoryQuery.contains(' ')) {
-    //   final query = repositoryQuery.replaceAll(' ', '+');
-    //   repositoryQuery = query;
-    // }
-
-    final apiUrl = generateURLFromSearchQuery(repositoryQuery, page, itemsPerPage);
-    final apiUri = Uri.parse(apiUrl);
-    final response = await http.get(apiUri);
-
-    if (response.statusCode != 200) {
-      throw Exception('No Repository: $repositoryQuery');
-    }
-
-    final jsonData = json.decode(response.body) as Map<String, dynamic>;
+    final apiUrl =
+        generateURLFromSearchQuery(repositoryQuery, page, itemsPerPage);
+    final jsonData = await apiClient.fetchData(apiUrl);
     final repositories = jsonData['items'] as List<dynamic>;
     return repositories
         .map(
